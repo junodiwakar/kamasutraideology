6d0782f77b04e68fc99daffa3a2fd490445b456f
junodiwakar <junodiwakar@gmail.com>
2024-06-12 19:10:22 +0530

    Created blog model and setup rich text

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,29 @@
+class BlogsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to @blog
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:headline, :description)
+  end
+end
