ae1cb358818df8b9eff4a77ad51969397577bb75
junodiwakar <junodiwakar@gmail.com>
2024-10-12 13:42:31 +0530

    Created blog edit destroy functionality

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -1,6 +1,53 @@
 class Admin::BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit,:update,:destroy]
+  before_action :set_categories, only: [:new,:create, :edit, :update]
+
   def index
     @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to @blog, notice: "blog created successfully!"
+    else
+      flash[:alert] = "Failed to create a blog."
+      redirect_to new_admin_blog_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blog_path(@blog), notice: 'Blog was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to admin_dashboard_path, notice: 'Blog was successfully destroyed.'
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:headline, :description, :slug, :image, category_ids: [])
+  end
+
+  def set_blog
+    @blog = Blog.find_by!(slug: params[:id])
+  end
+
+  def set_categories
+    @categories =  Category.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-  resources :blogs
+  resources :blogs, only: [:index, :show]
 
   resources :categories
   namespace :admin do
     get 'dashboard', to: 'dashboard#index'
-    resources :blogs, except:[:show]
+    resources :blogs, except: [:index, :show]
   end
 end
